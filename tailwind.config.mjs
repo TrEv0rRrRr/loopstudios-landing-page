c1336b5ff5cab147e52459b1d367b6c96baba926
Trevor <valentinosolischang@gmail.com>
2024-12-28 20:20:02 -0500

    Add layout components and styles for Loopstudios landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "Dark-Gray": "hsl(0, 0%, 55%)",
+        "Very-Dark-Gray": "hsl(0, 0%, 41%)",
+      },
+      fontWeight: {
+        WeightAlata: "400",
+        WeightJosefin: "300",
+      },
+      fontFamily: {
+        AlataF: "Alata",
+        JosefinF: ['"Josefin Sans"'],
+      },
+    },
+  },
+  plugins: [],
+};
